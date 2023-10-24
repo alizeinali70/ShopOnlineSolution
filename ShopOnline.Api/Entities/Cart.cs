965a25584c6da20fc8b383bcc447ed1de367c5e2
ali zeinali <ali.zeinali70@gmail.com>
2023-10-24 22:51:48 +0200

    Add project files.

---
+++ ShopOnline.Api/Entities/Cart.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopOnline.Api.Entities
+{
+    public class Cart
+    {
+        /// <summary>
+        /// Has Relation One to Many to CartItem
+        /// </summary>
+        public int Id { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ ShopOnline.Api/Entities/ProductCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopOnline.Api.Entities
+{
+    public class ProductCategory
+    {/// <summary>
+     /// Has Relation One to Many to Product    
+     /// </summary>
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
